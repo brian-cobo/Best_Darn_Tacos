e1aa6fd02baa57819487e7403f7606cda5028ebf
Brian Cobo <cobo.brian97@gmail.com>
2019-10-20 00:50:13 -0500

    updating function to return geopandas dataframe

--- Program_Functions.py
+++ Program_Functions.py
@@ -6,9 +6,11 @@ import geopandas
 def load_data():
     data = pd.read_csv('Cleaned_Tacos.csv')
     data = geopandas.GeoDataFrame(data, geometry=geopandas.points_from_xy(data.longitude, data.latitude))
+    return data
 
 def get_distance_from_current_location(point):
     g = geocoder.ip('me')
     lat, long = g.latlng
     current_location = Point(lat, long)
     return current_location.distance(point)
+
